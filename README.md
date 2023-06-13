f78b15428f711d67a9395596609eed1aa0472d53
lyubick <andrejs.lubimovs@gmail.com>
2023-06-13 09:53:44 +0300

    NEW. Support for directories, multiple files. Docs.

--- README.md
+++ README.md
@@ -1,4 +1,38 @@
 # action-YAML-schema-validator
-## Overview
 
-Work in progress...
+## Overview
+This action can validate YAML files against provided JSON Schema. In case if YAML file is in full compliance
+with provided JSON Schema action will exit gracefully, crash (unhandled exception) will occur otherwise.
+
+## Usage
+To use action one can use code snippet below.
+```yaml
+jobs:
+  my_cool_validation:
+    runs-on: ubuntu-latest
+    steps:
+      - uses: actions/checkout@v2
+      - name: Check my YAML file with my JSON schema
+        id: validation
+        uses: lyubick/action-YAML-schema-validator@v1
+        with:
+          json-schema-file: path/to/my/cool/schema.json
+          yaml-file-dir: path/to/my/cool/yaml/file.yaml
+```
+One should provide two parameters:
+- `json-schema-file`, points to legit JSON Schema file
+- `yaml-file-dir`, is a comma separated list that contains
+  - Single YAML files
+  - Directories that will be parsed for `.yaml` and `.yml` files
+
+## Results
+### Success
+In case of success action will end gracefully (exit code 0).
+
+### Failure
+In case of failure action will end with crash (unhandled exception), like on example below:
+```text
+File `invalid/yaml/file/here.yaml` failed validation with >>>`failure reason here`<<<
+
+Traceback ...
+```
--- validator.py
+++ validator.py
@@ -4,6 +4,7 @@ import sys
 
 import yaml
 from jsonschema import validate
+from jsonschema.exceptions import ValidationError
 
 if __name__ == '__main__':
     args = sys.argv[1:]
@@ -15,18 +16,40 @@ if __name__ == '__main__':
             with open(args[0], 'r') as stream:
                 schema = json.loads("".join(stream.readlines()))
         else:
-            raise f'Provided file `{schema_file}` is not a file! Please provide legit JSON Schema file.'
+            raise f'Provided JSON Schema is not a file! Please provide legit JSON Schema file.'
     else:
-        raise f'Provided file `{schema_file}` does not exist!'
+        raise f'Provided JSON Schema file does not exist!'
 
-    yaml_file = args[1]
+    yaml_input = list(args[1].split(','))
 
-    if os.path.exists(schema_file):
-        if os.path.isfile(schema_file):
-            with open(yaml_file, 'r') as stream:
-                json = json.loads(json.dumps(yaml.safe_load(stream)))
-            validate(instance=json, schema=schema)
+    yaml_files = []
+    for yaml_object in yaml_input:
+        if os.path.isdir(yaml_object):
+            yaml_files.extend(
+                list(
+                    map(
+                        lambda f: yaml_object + ('/' if not yaml_object.endswith('/') else '') + f,
+                        filter(
+                            lambda f: str(f).endswith('.yaml') or str(f).endswith('.yml'),
+                            os.listdir(yaml_object)
+                        )
+                    )
+                )
+            )
+        elif os.path.isfile(yaml_object):
+            yaml_files.append(yaml_object)
+
+    for yaml_file in yaml_files:
+        if os.path.exists(yaml_file):
+            if os.path.isfile(yaml_file):
+                with open(yaml_file, 'r') as stream:
+                    yaml_json = json.loads(json.dumps(yaml.safe_load(stream)))
+                try:
+                    validate(instance=yaml_json, schema=schema)
+                except ValidationError as exc:
+                    print(f'File `{yaml_file}` failed validation with >>>`{exc}`<<<', file=sys.stderr)
+                    raise exc
+            else:
+                raise f'Provided YAML file is not a file! Please provide legit YAML file.'
         else:
-            raise f'Provided `{yaml_file}` is not a file! Please provide legit YAML file.'
-    else:
-        raise f'Provided `{yaml_file}` does not exist!'
+            raise f'Provided YAML file does not exist!'
