355b15a2f23c567302749b831e93ea2eb190631d
lyubick <andrejs.lubimovs@gmail.com>
2023-06-13 08:57:37 +0300

    FIX. Minor fixes and improvements.

--- validator.py
+++ validator.py
@@ -1,4 +1,5 @@
 import json
+import os.path
 import sys
 
 import yaml
@@ -7,10 +8,25 @@ from jsonschema import validate
 if __name__ == '__main__':
     args = sys.argv[1:]
 
-    with open(args[0], 'r') as stream:
-        schema = json.loads("".join(stream.readlines()))
+    schema_file = args[0]
 
-    with open(args[1], 'r') as stream:
-        json = json.loads(json.dumps(yaml.safe_load(stream)))
+    if os.path.exists(schema_file):
+        if os.path.isfile(schema_file):
+            with open(args[0], 'r') as stream:
+                schema = json.loads("".join(stream.readlines()))
+        else:
+            raise f'Provided file `{schema_file}` is not a file! Please provide legit JSON Schema file.'
+    else:
+        raise f'Provided file `{schema_file}` does not exist!'
 
-    validate(instance=json, schema=schema)
+    yaml_file = args[1]
+
+    if os.path.exists(schema_file):
+        if os.path.isfile(schema_file):
+            with open(yaml_file, 'r') as stream:
+                json = json.loads(json.dumps(yaml.safe_load(stream)))
+            validate(instance=json, schema=schema)
+        else:
+            raise f'Provided `{yaml_file}` is not a file! Please provide legit YAML file.'
+    else:
+        raise f'Provided `{yaml_file}` does not exist!'
